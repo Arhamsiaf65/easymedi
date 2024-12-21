b4242ebed02ba659f09c26f56a4aff11b108c32b
ArhamSaif <146959684+Arhamsiaf65@users.noreply.github.com>
2024-12-21 23:09:55 +0500

    Update tailwind.config.js

--- Admin/tailwind.config.js
+++ Admin/tailwind.config.js
@@ -1,11 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+        extend: {
+          animation: {
+            wiggle: 'wiggle 1s ease-in-out infinite',
+          },
+          keyframes: {
+            wiggle: {
+              '0%, 100%': { transform: 'translateX(0)' },
+              '50%': { transform: 'translateX(10px)' },
+            },
+          },
+        },
+      },
   plugins: [],
 }
